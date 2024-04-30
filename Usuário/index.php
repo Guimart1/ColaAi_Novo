496ec84e2db7ffe50675b25a710455b1e804ce70
Otávio <pizza131@outlook.com>
2024-04-29 23:04:07 -0300

    Index do usuario

---
+++ Usuário/index.php
@@ -0,0 +1,135 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cola Aí</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css'>
+    <link rel="stylesheet" href="../css/styleUsuarioCadastro.css">
+</head>
+
+<body style="background-color: #FFFBE7; width: 100%">
+
+    <!-- Bolinhas conflitam com os botões
+    <div class="d-flex" style="height: 100vh; width: 100%; position:absolute;">
+        <div class="d-flex justify-content-center align-items-end">
+            <img src="../img/Usuario/Bolinhas_index/Inferior_Esquerdo.png" style="height:9vw; position:fixed; margin-left:10px"></img>
+        </div>
+        <div class="d-flex justify-content-start align-items-start" >
+            <img src="../img/Usuario/Bolinhas_index/Superior_esquerdo.png" style="height:6vw"></img>
+        </div>
+        <div class="d-flex w-100 justify-content-center align-items-end">
+            <img src="../img/Usuario/Bolinhas_index/Inferior_Centro.png" style="height:5vw; margin-left:25%; position:fixed;"></img>
+        </div>
+        <div class="d-flex w-100 justify-content-center align-items-start">
+            <img src="../img/Usuario/Bolinhas_index/Superior_Centro.png" style="height:6vw; margin-right:50%"></img>
+        </div>
+        <div class="d-flex justify-content-end align-items-start">
+            <img src="../img/Usuario/Bolinhas_index/Centro_Direito.png" style="height:10vw; margin-top:10%; position:absolute"></img>
+        </div>
+        <div class="d-flex justify-content-end align-items-end">
+            <img src="../img/Usuario/Bolinhas_index/Centro_Direito_2.png" style="height:10vw; margin-bottom:10%; position:fixed"></img>
+        </div>
+        
+    </div>
+    -->
+
+    <div class="col-md-12 d-flex justify-content-end" style="height: 100px; position:fixed; bottom:10px; padding:5%;">
+        <li style="list-style: none;">
+            <a href="#topo"><img src="../img/Usuario/scroll.png" style="width: 75px; height: 75px" alt="Crianças pulando"></a></img>
+        </li>
+    </div>
+
+    <div id="topo" class="row w-100 d-flex justify-content-center align-items-center" style="height: 100vh; width: 100%;">
+        <div class="col-md-7 d-flex justify-content-center align-items-center">
+            <img src="../img/Usuario/index_img_1.png" class="img-fluid" style="width: 75%;" alt="Crianças pulando">
+        </div>
+        <div class="col-md-5 d-flex justify-content-center">
+            <div class="d-flex flex-column justify-content-center align-items-center">
+                <img src="../img/Usuario/colaai.png" style="height: 100%; margin-Bottom: 50px" alt="Logo Cola Aí">
+                <div class="buttonSearch col-2 d-flex justify-content-center mt-auto mb-5">
+                    <a href="./Login/index.php">
+                        <button type="submit" class="border-0" style="width: 5vw;">
+                            <h1 class="text-center"><b style="color: #E6AEB0;">Lo</b><b style="color: #6DA2B1;">gin</b></h1>
+                        </button>
+                    </a>
+                </div>
+                <div class="buttonSearch col-2 d-flex justify-content-center mt-auto mb-5">
+                    <a href="./Cadastro/index.php">
+                        <button type="submit" class="border-0" style="width: 5vw;">
+                            <h1 class="text-center"><b style="color: #E6AEB0;">Ca</b><b style="color: #6DA2B1;">das</b><b style="color: #93CC4C;">tre</b><b style="color: #FFD417;">-se</b></h1>
+                        </button>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row w-100 d-flex justify-content-center align-items-center " style="height: 80vh;">
+        <div class="col-md-5 d-flex justify-content-center">
+            <div class="col-9 d-flex justify-content-center">
+                <h4 class="text-center"><p style="color: #A6A6A6; text-align:justify" class="lh-lg">
+                    A cultura é extremamente importante para o crescimento da sua criança, e nós te ajudamos nisso!
+                    <img src="../img/Usuario/logoColaai.png" style="height: 45px; margin-bottom: 20px" alt="Cola Aí">
+                    é um site que te mostra os eventos culturais mais próximos de você, agora é só entrar e aproveitar.
+                </p></h4>
+            </div>
+        </div>
+        <div class="col-md-7 d-flex justify-content-center align-items-center">
+            <img src="../img/Usuario/index_img_2.png" class="img-fluid" style="width: 75%;" alt="Crianças pulando">
+        </div>
+    </div>
+    <div class="row w-100 d-flex justify-content-center align-items-center " style="height: 80vh;">
+        <div class="col-md-7 d-flex justify-content-center align-items-center">
+            <img src="../img/Usuario/index_img_3.png" class="img-fluid" style="width: 50%;" alt="Crianças pulando">
+        </div>
+        <div class="col-md-5 d-flex justify-content-center">
+            <div class="col-9 d-flex justify-content-center">
+                <h4 class="text-center "><p style="color: #A6A6A6; text-align:justify" class="lh-lg">
+                    Acesse pelo seu celular, computador, tablet ou notebook. nós do
+                    <img src="../img/Usuario/logoColaai.png" style="height: 45px; margin-bottom: 20px" alt="Cola Aí">
+                    estamos te esperando para participar da nossa comunidade de responsaveis que querem dar o melhor da cultura para os seus pequenos!
+                </p></h4>
+            </div>
+        </div>
+    </div>
+
+    <footer class="w-100 h-auto d-flex justify-content-center">
+        <div class="row d-flex align-items-start pt-4 g-4 text-start" style="width: 80%;">
+            <div class="col-md-4">
+                <img src="../img/Login/Cola AI logo.png" alt="" style="width: 40%;" class="mb-2">
+                <p style="font-size:1em; font-weight:bold; color: #A6A6A6;">Seja bem-vindo(a) a Cola ai, nós pretendemos lhe ajudar a encontrar as
+                    melhores experiências para suas crianças.</p>
+            </div>
+            <div class="col-md-2">
+                <h4 style="color: #6D9EAF;">Infos</h4>
+                <ul class="m-0 p-0" style="list-style: none; font-weight: bold; cursor:pointer">
+                    <li><a class="dropdown-item fw-bold" onclick="modalSobre(0,0)">Sobre</a></li>
+                    <li><a class="dropdown-item fw-bold" onclick="modalFeedback(0,0)">Feedback</a></li>
+                    <li><a class="dropdown-item fw-bold" onclick="modalContato(0,0)">Contato</a></li>
+                </ul>
+            </div>
+            <div class="col-md-3">
+                <h4 style="color: #6D9EAF;">Desenvolvedora</h4>
+                <img src="../img/Usuario/magma-logo.png" alt="" style="width: 70%;">
+            </div>
+            <div class="col-md-3">
+                <h4 style="color: #6D9EAF;">Acesso rápido</h4>
+                <ul class="m-0 p-0" style="list-style: none; font-weight: bold">
+                    <li>Início</li>
+                    <li>Teatros</li>
+                    <li>Parques</li>
+                    <li>Museus</li>
+                    <li>Centro Culturais</li>
+                    <li>CEU</li>
+                </ul>
+            </div>
+        </div>
+    </footer>
+    <script type="text/javascript" src="http://code.jquery.com/jquery-3.0.0.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous" defer>
+    </script>
+</body>
+
+</html>
