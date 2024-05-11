86b5ec1fad4602548d3ba1e23c941754409eaff5
Otávio <pizza131@outlook.com>
2024-05-11 15:33:09 -0300

    Front perfil usuário finalizado

--- js/modal.js
+++ js/modal.js
@@ -64,6 +64,33 @@
             document.getElementById($elemento).value = $id;
             //window.location.href = "./registro.php";
         }
+
+		function modalFotoPerfil($id, $elemento) {
+            const myModal = new bootstrap.Modal('#modalFotoPerfil');
+            myModal.show();
+            document.getElementById($elemento).value = $id;
+            //window.location.href = "./registro.php";
+        }
+
+		function modalBannerPerfil($id, $elemento) {
+            const myModal = new bootstrap.Modal('#modalBannerPerfil');
+            myModal.show();
+            document.getElementById($elemento).value = $id;
+            //window.location.href = "./registro.php";
+        }
+
+		function modalDadosPessoais($id, $elemento) {
+            const myModal = new bootstrap.Modal('#modalDadosPessoais');
+            myModal.show();
+            document.getElementById($elemento).value = $id;
+            //window.location.href = "./registro.php";
+        }
+		function modalSeguindo($id, $elemento) {
+            const myModal = new bootstrap.Modal('#modalSeguindo');
+            myModal.show();
+            document.getElementById($elemento).value = $id;
+            //window.location.href = "./registro.php";
+        }
   $(document).ready(function() {
     $('#modalPadrao').modal('show');
   });
