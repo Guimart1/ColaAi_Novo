7c5ae221fa02b881477e56a48eba22eb042bc46b
Phaniee <stephanie.silva185@etec.sp.gov.br>
2024-05-20 19:31:46 -0300

    parte de aparecer todos eventos mostrando a quantidades de eventos cadastrados na plataforma e todos os eventos que estão ativos

--- Usuário/TodosEventos/index.php
+++ Usuário/TodosEventos/index.php
@@ -1,51 +1,61 @@
-<?php 
-    require_once '../../dao/EventoDao.php';
-    
-    $eventos = EventoDao::selectAll();
+<?php
+require_once '../../dao/EventoDao.php';
 
-    $enderecosEventos= [];
+$eventos = EventoDao::selectAllActive();
 
-    $eventos_js = [];
+$enderecosEventos = [];
 
-    $evento_coords = [];
+$eventos_js = [];
+
+$evento_coords = [];
 
 
-    $i = 0;
+$i = 0;
 
-    foreach ($eventos as $evento) {
-        // Formate os dados do evento como desejado
-        $enderecoEvento = $evento['enderecoEvento'] . "," . $evento['numeroEvento'] . "," .  $evento['complementoEvento']. "," .  $evento['bairroEvento'] . "," . $evento['cepEvento'] . "," . $evento['cidadeEvento']. "," .  $evento['ufEvento'];
+foreach ($eventos as $evento) {
+    // Formate os dados do evento como desejado
+    $enderecoEvento = $evento['enderecoEvento'] . "," . $evento['numeroEvento'] . "," .  $evento['complementoEvento'] . "," .  $evento['bairroEvento'] . "," . $evento['cepEvento'] . "," . $evento['cidadeEvento'] . "," .  $evento['ufEvento'];
 
-        $enderecosEventos[$i] = $enderecoEvento;
-        $i++;
-        
-    }
+    $enderecosEventos[$i] = $enderecoEvento;
+    $i++;
+}
 
-    var_dump($enderecosEventos);
+//var_dump($enderecosEventos);
 
-    // foreach ($enderecosEventos as $eventoss) {
-    //     echo $eventoss;
-    // }
+// foreach ($enderecosEventos as $eventoss) {
+//     echo $eventoss;
+// }
 
-    
 
-    // Converta cada evento em um formato JavaScript
-    // foreach ($eventos as $evento) {
-    //     // Formate os dados do evento como desejado
-    //     $evento_js = [
-    //         'nome' => $evento['nomeEvento'],
-    //         'latitude' => $evento['latitude'], // Substitua por sua coluna de latitude
-    //         'longitude' => $evento['longitude'], // Substitua por sua coluna de longitude
-    //         // Adicione outros campos conforme necessário
-    //     ];
 
-    //     // Adicione o evento ao array de eventos JavaScript
-    //     $eventos_js[] = $evento_js;
-    // }
-    ?>
-<html lang="pt-br" class="hydrated"><head>
+// Converta cada evento em um formato JavaScript
+// foreach ($eventos as $evento) {
+//     // Formate os dados do evento como desejado
+//     $evento_js = [
+//         'nome' => $evento['nomeEvento'],
+//         'latitude' => $evento['latitude'], // Substitua por sua coluna de latitude
+//         'longitude' => $evento['longitude'], // Substitua por sua coluna de longitude
+//         // Adicione outros campos conforme necessário
+//     ];
 
-    <meta charset="UTF-8"><style data-styles="">ion-icon{visibility:hidden}.hydrated{visibility:inherit}</style>
+//     // Adicione o evento ao array de eventos JavaScript
+//     $eventos_js[] = $evento_js;
+// }
+?>
+<html lang="pt-br" class="hydrated">
+
+<head>
+
+    <meta charset="UTF-8">
+    <style data-styles="">
+        ion-icon {
+            visibility: hidden
+        }
+
+        .hydrated {
+            visibility: inherit
+        }
+    </style>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home - Cola Aí</title>
     <link rel="stylesheet" href="../../css/styleAdm.css">
@@ -72,71 +82,72 @@
 <body class="fundo-bolinha">
 
     <?php
-        require_once('../Componentes/headerLogado.php')
+    require_once('../Componentes/headerLogado.php')
     ?>
 
 
-<div class="container-fluid">
-<div class="row">
-        <div class="col-md-2 filtroBox pt-3" id="filtro">
-            <div class="filtroEtaria mb-2">
-                <label for="" class="fs-5">Faixa Etária</label>
-                <div class="mt-1 mb-1">
-                    <input type="number" class="faixaEtariaInput me-2" min="1" max="15"><span style="color: #6D9EAF;">min</span>
-                </div>
-                <div class="mt-1 mb-1">
-                    <input type="number" class="faixaEtariaInput me-2" min="1" max="15"><span style="color: #E6AEB2">max</span>
-                </div>
-            </div>
-            <div class="distanciaBox">
-                <label for="" class="mb-1 fs-5 mb-4">Distância</label>
-                <div class="d-flex align-items-center">
-                    <div class="teste"></div><input type="range" name="" id=""><div class="teste"></div>
-                </div>
-            </div>
-            <span class="ms-auto">km</span>
-            <div class="turnoBox mt-2">
-                <label for="" class="fs-5">Turno</label>
-                <div class="inputFiltros">
-                    <input type="checkbox" name="" id=""><span class="ms-2">Todos</span>
-                </div>
-                <div class="inputFiltros">
-                    <input type="checkbox" name="" id=""><span class="ms-2">Vespertino</span>
-                </div>
-                <div class="inputFiltros">
-                    <input type="checkbox" name="" id=""><span class="ms-2">Matutino</span>
-                </div>
-                <div class="inputFiltros">
-                    <input type="checkbox" name="" id=""><span class="ms-2">Noturno</span>
-                </div>
-            </div>
-
-            <div class="turnoBox mt-1">
-                <label for="" class="fs-5 ">Valor</label>
-                <div class="inputFiltros">
-                    <input type="checkbox" name="" id=""><span class="ms-2">Gratuito</span>
-                </div>
-                <div class="inputFiltros">
-                    <input type="checkbox" name="" data-bs-toggle="collapse" href="#collapsePreco" role="button" aria-expanded="false" aria-controls="collapseExample"><span class="ms-2">Pago</span>
-                </div>
-                <div class="collapse" id="collapsePreco">
-                    <li class=" w-100" style="list-style: none;">
-                    <div class="d-flex flex-column">
-                        <label for="" class="mb-1 fs-5">Preço</label>
-                        <div class="d-flex">
-                            <span style="color: #6D9EAF;">min</span><input type="number" class="faixaEtariaInput ms-2" min="1" max="15">
-                            <span class="ms-2" style="color:#E6AEB2">max</span><input type="number" class="faixaEtariaInput ms-2" min="1" max="15">
-                        </div>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-2 filtroBox pt-3" id="filtro">
+                <div class="filtroEtaria mb-2">
+                    <label for="" class="fs-5">Faixa Etária</label>
+                    <div class="mt-1 mb-1">
+                        <input type="number" class="faixaEtariaInput me-2" min="1" max="15"><span style="color: #6D9EAF;">min</span>
                     </div>
-                    </li> 
+                    <div class="mt-1 mb-1">
+                        <input type="number" class="faixaEtariaInput me-2" min="1" max="15"><span style="color: #E6AEB2">max</span>
+                    </div>
+                </div>
+                <div class="distanciaBox">
+                    <label for="" class="mb-1 fs-5 mb-4">Distância</label>
+                    <div class="d-flex align-items-center">
+                        <div class="teste"></div><input type="range" name="" id="">
+                        <div class="teste"></div>
+                    </div>
+                </div>
+                <span class="ms-auto">km</span>
+                <div class="turnoBox mt-2">
+                    <label for="" class="fs-5">Turno</label>
+                    <div class="inputFiltros">
+                        <input type="checkbox" name="" id=""><span class="ms-2">Todos</span>
+                    </div>
+                    <div class="inputFiltros">
+                        <input type="checkbox" name="" id=""><span class="ms-2">Vespertino</span>
+                    </div>
+                    <div class="inputFiltros">
+                        <input type="checkbox" name="" id=""><span class="ms-2">Matutino</span>
+                    </div>
+                    <div class="inputFiltros">
+                        <input type="checkbox" name="" id=""><span class="ms-2">Noturno</span>
+                    </div>
+                </div>
+
+                <div class="turnoBox mt-1">
+                    <label for="" class="fs-5 ">Valor</label>
+                    <div class="inputFiltros">
+                        <input type="checkbox" name="" id=""><span class="ms-2">Gratuito</span>
+                    </div>
+                    <div class="inputFiltros">
+                        <input type="checkbox" name="" data-bs-toggle="collapse" href="#collapsePreco" role="button" aria-expanded="false" aria-controls="collapseExample"><span class="ms-2">Pago</span>
+                    </div>
+                    <div class="collapse" id="collapsePreco">
+                        <li class=" w-100" style="list-style: none;">
+                            <div class="d-flex flex-column">
+                                <label for="" class="mb-1 fs-5">Preço</label>
+                                <div class="d-flex">
+                                    <span style="color: #6D9EAF;">min</span><input type="number" class="faixaEtariaInput ms-2" min="1" max="15">
+                                    <span class="ms-2" style="color:#E6AEB2">max</span><input type="number" class="faixaEtariaInput ms-2" min="1" max="15">
+                                </div>
+                            </div>
+                        </li>
+                    </div>
+                </div>
+                <div class="w-100 d-flex justify-content-center mt-3">
+                    <button class="btnFiltro rounded rounded-3" onclick="Fechar()">Aplicar filtros</button>
                 </div>
             </div>
-            <div class="w-100 d-flex justify-content-center mt-3">
-                <button class="btnFiltro rounded rounded-3" onclick="Fechar()">Aplicar filtros</button>
-            </div>
-        </div>
 
-        <div class="listSearch pt-3 pb-3">
+            <!-- <div class="listSearch pt-3 pb-3">
                 <div class="searchBox d-flex justify-content-center">
                     <div class="searchInput position-relative">
                         <img src="../../img/Usuario/icon-search.png" alt="">
@@ -151,51 +162,45 @@
                         <button type="submit" class="border-0 rounded-3" onclick="Abrir()">Filtros</button>
                     </div>
                 </div>
-            </div>
-        <div class="col-md-10 listEventos">
+            </div> -->
+            <div class="col-md-10 listEventos">
 
-            <div id="map"></div>
+                <div id="map"></div>
 
 
-            <!-- Card se não encontrar evento -->
+                <!-- Card se não encontrar evento -->
 
-            <!-- <div class="d-flex justify-content-center">
+                <!-- <div class="d-flex justify-content-center">
                 <div class="boxVazio p-3 fw-bold rounded rounded-5">
                     <p>Infelizmente não encontramos eventos próximos a este endereço  cadastrados.</p>
                 </div>
-            </div>
+            </div> -->
 
-            
-            <!-- Pode copiar essa div e só mudar o titulo pras outras funções de pesquisa -->
-            <div class="p-4">
-                
-                <h2 class="mb-3">Todos os Eventos (10)</h2>
-                <!-- <h2 class="mb-3">Mais Eventos</h2> -->
-                <!-- <h2 class="mb-3">Todos os Eventos Próximos</h2> -->
-                <div class="row g-3">
-                    <!--Card de evento-->
-                    <div class="col-sm-6 col-lg-3">
-                        <a href="../Evento/"> <!-- Adicione o link aqui -->
-                            <div class="imageBox position-relative" style="width: 100%; height: 200px;"> <!-- Defina a largura e altura desejadas -->
-                                <img src="../../img/Usuario/carrossel-padrao.png" alt="Imagem do evento" style="width: 100%; height: 100%;"> <!-- Defina a largura e altura desejadas -->
-                                <div class="descMini p-2 ps-4"> <!-- Defina a largura igual à da imagem -->
-                                    <h3 class="fs-4 tituloEvento">Nome Evento</h3>
-                                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
-                                </div>
+
+                <!-- Pode copiar essa div e só mudar o titulo pras outras funções de pesquisa -->
+                <div class="p-4">
+                    <h2 class="mb-3">Todos os Eventos (<?php echo count($eventos); ?>)</h2>
+                    <div class="row g-3">
+                        <?php foreach ($eventos as $evento) : ?>
+                            <div class="col-sm-6 col-lg-3">
+                                <a href="../Evento/evento.php?id=<?php echo $evento['idEvento']; ?>"> <!-- Adicione o link aqui -->
+                                    <div class="imageBox position-relative" style="width: 100%; height: 200px;"> <!-- Defina a largura e altura desejadas -->
+                                        <img src="../../img/Organizacao/<?= $evento['imagemEvento']; ?>" alt="Imagem do Evento" style="width: 100%; height: 100%;"> <!-- Defina a largura e altura desejadas -->
+                                        <div class="descMini p-2 ps-4"> <!-- Defina a largura igual à da imagem -->
+                                            <h3 class="fs-4 tituloEvento"><?php echo $evento['nomeEvento']; ?></h3>
+                                            <p><?php echo $evento['descEvento']; ?></p>
+                                        </div>
+                                    </div>
+                                </a>
                             </div>
-                        </a>
+                        <?php endforeach; ?>
                     </div>
                 </div>
             </div>
-            
         </div>
     </div>
-</div>
-    <script
-      src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAEx8FgHwmJjDQPZq_JQBPxi_1on_zcpQI&callback=initMap&v=weekly&solution_channel=GMP_CCS_geolocation_v1"
-      defer
-    ></script>
-    
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAEx8FgHwmJjDQPZq_JQBPxi_1on_zcpQI&callback=initMap&v=weekly&solution_channel=GMP_CCS_geolocation_v1" defer></script>
+
     <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
     <script nomodule="" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
     <script type="text/javascript" src="../../js/personalizar.js"></script>
@@ -203,27 +208,30 @@
     <script type="text/javascript" src="../../js/modal.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous">
     </script>
-        <script>  
+    <script>
         let nav = document.getElementById("nav")
         var i = 0
-        function toggleNav(){
+
+        function toggleNav() {
             if (i == 0) {
                 nav.classList.add("navbarActive-on");
                 nav.classList.remove("navbarActive-off");
                 i = 1
-            }else {
+            } else {
                 nav.classList.add("navbarActive-off");
                 nav.classList.remove("navbarActive-on");
                 i = 0
             }
         }
-        </script>
+    </script>
     <script>
         let filtro = document.getElementById("filtro")
+
         function Abrir() {
             filtro.classList.add("filtroBox-on");
             filtro.classList.remove("filtroBox-off");
         }
+
         function Fechar() {
             filtro.classList.add("filtroBox-off");
             filtro.classList.remove("filtroBox-on");
@@ -231,7 +239,6 @@
     </script>
 
     <script>
-
         const getPosition = position => {
             const dados = {
                 latitude: position.coords.latitude,
@@ -243,7 +250,7 @@
         }
 
         const geoError = error => {
-            console.log ("Erro: " + error.message)
+            console.log("Erro: " + error.message)
         }
         navigator.geolocation.getCurrentPosition(getPosition, geoError)
 
@@ -253,7 +260,10 @@
 
         function initMap() {
             map = new google.maps.Map(document.getElementById("map"), {
-                center: { lat: -34.397, lng: 150.644 },
+                center: {
+                    lat: -34.397,
+                    lng: 150.644
+                },
                 zoom: 12,
             });
             infoWindow = new google.maps.InfoWindow();
@@ -290,10 +300,9 @@
             // Solicita a localização do usuário
             navigator.geolocation.getCurrentPosition(getPosition, geoError);
         }
-
     </script>
 
-    
+
 
     <!-- <script>
         // Adicione os eventos ao mapa usando JavaScript
@@ -312,6 +321,8 @@
         });
     </script> -->
 
-    
 
-</body></html>
+
+</body>
+
+</html>
