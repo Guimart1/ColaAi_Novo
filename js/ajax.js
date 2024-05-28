76473f0f324e2dead9d8321e6a46985fab3419b8
bruno2yah <bruno.geanini04@gmail.com>
2024-05-28 17:35:13 -0300

    informaçoes do feedback

--- js/ajax.js
+++ js/ajax.js
@@ -14,3 +14,19 @@ function enviarIdEvento(idEvento) {
         }
     });
 }
+function enviarIdFeedback(idFeedback) {
+    // Faz a requisição AJAX
+    $.ajax({
+        url: '', // URL do seu script PHP
+        method: 'POST',
+        data: { idFeedback: idFeedback }, // Envia o ID do evento para o PHP
+        success: function(data) {
+            // Manipule a resposta do servidor, se necessário
+            console.log(data);
+            $('#informacoes').html(data); // Insere o conteúdo na tbody da tabela
+        },
+        error: function(error) {
+            console.error('Erro:', error);
+        }
+    });
+}
