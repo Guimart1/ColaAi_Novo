9d3951d9933246e03afdb1f6f98cab08ebe755b4
Phaniee <stephanie.silva185@etec.sp.gov.br>
2024-06-05 11:00:10 -0300

    arrumei o cadastro do user, ainda falta foto banner e perfil

--- Usuário/Cadastro/index.php
+++ Usuário/Cadastro/index.php
@@ -11,6 +11,7 @@
 </head>
 
 <body style="background-color: #FFFBE7">
+
     <div class="col-md-12 ps-3 mt-2" style="height: 10vh;">
         <img src="../../img/Login/Cola AI logo.png" style="height: 70%;" alt="Logo Cola Aí">
     </div>
@@ -21,25 +22,93 @@
         </div>
         <div class="col-md-5 d-flex justify-content-center">
             <div class="formulario rounded-5  p-3 rounded d-flex flex-column sombra">
+                <!--Titulo-->
                 <h1 class="text-center mb-5"><b style="color: #E6AEB0;">Ca</b><b style="color: #6DA2B1;">das</b><b style="color: #93CC4C;">tre</b><b style="color: #FFD417;">-se</b></h1>
-                <form method="post" id="formulario">
+                <form method="post" action="process.php" id="formulario" onsubmit="return validarSenha();">
 
+                    <!--Nome e sobrenome-->
                     <div class="col-12 d-flex flex-row mb-4">
                         <input required type="text" placeholder="Nome" id="nome" name="nomeUsuario" class="input-group me-4">
                         <input required type="text" placeholder="Sobrenome" id="sobrenome" name="sobrenomeUsuario" class="input-group">
                     </div>
+
+                    <!--Email-->
                     <input required type="email" placeholder="Email" id="email" name="emailUsuario" class="input-group mb-4">
+
+                    <!--Celular-->
                     <input required type="tel" placeholder="Telefone" id="telefone" name="telUsuario" class="input-group mb-4" data-mask="(00)00000-0000">
+
+                    <!--Senha-->
                     <div class="orgSenha d-flex">
                         <input required type="password" placeholder="Senha" id="senha" name="senhaUsuario" class="input-group mb-4">
                         <i class="bi bi-eye col1" id="btnSenha" onclick="MostrarSenha()"></i>
                     </div>
+
+                    <!--Confirmar senha-->
                     <div class="orgSenha d-flex">
-                        <input required type="password" id="senha1" placeholder="Confirmar senha" name="confirmarSenha" class="input-group mb-4">
+                        <input required type="password" id="senha1" placeholder="Confirmar senha" name="confirmarSenhaUsuario" class="input-group mb-4">
                         <i class="bi bi-eye col1" id="btnSenha1" onclick="MostrarSenha2()"></i>
                     </div>
-                    <div id="mensagemErro" class="text-danger mb-3" style="display:none;"></div>
-                    <div id="mensagemErroSenha" class="text-danger mb-3" style="display:none;"></div>
+                    
+                    <script>
+                        function myFunction() {
+                            var x = document.getElementById("senha");
+                            var y = document.getElementById("senha1")
+                            if (x.type === "password") {
+                                x.type = "text";
+                                y.type = "text";
+                            } else {
+                                x.type = "password";
+                                y.type = "password";
+                            }
+                        }
+
+                        function MostrarSenha() {
+                            var inputPass = document.getElementById('senha')
+                            var btnShowPass = document.getElementById('btnSenha')
+
+                            if (inputPass.type === 'password') {
+                                inputPass.setAttribute('type', 'text')
+                                btnShowPass.classList.replace('bi-eye', 'bi-eye-slash')
+                            } else {
+                                inputPass.setAttribute('type', 'password')
+                                btnShowPass.classList.replace('bi-eye-slash', 'bi-eye')
+                            }
+                        }
+
+                        function MostrarSenha2() {
+                            var inputPass = document.getElementById('senha1')
+                            var btnShowPass = document.getElementById('btnSenha1')
+
+                            if (inputPass.type === 'password') {
+                                inputPass.setAttribute('type', 'text')
+                                btnShowPass.classList.replace('bi-eye', 'bi-eye-slash')
+                            } else {
+                                inputPass.setAttribute('type', 'password')
+                                btnShowPass.classList.replace('bi-eye-slash', 'bi-eye')
+                            }
+                        }
+
+                        function validarSenha() {
+                            var senha = document.getElementById('senha').value;
+                            var confirmarSenha = document.getElementById('senha1').value;
+                            var regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+
+                            if (!regex.test(senha)) {
+                                alert('A senha deve ter no mínimo 8 caracteres, incluindo pelo menos uma letra maiúscula, uma letra minúscula, um número e um caractere especial.');
+                                return false;
+                            }
+
+                            if (senha !== confirmarSenha) {
+                                alert('As senhas não coincidem.');
+                                return false;
+                            }
+
+                            return true;
+                        }
+                    </script>
+
+                    <!--Confirmar-->
                     <div class="col-12 d-flex justify-content-end">
                         <button id="botao" type="submit" class="botao btn fs-5" name="acao" value="SALVAR">Cadastre-se</button>
                     </div>
@@ -48,68 +117,13 @@
         </div>
     </div>
 
-    <script>
-        function MostrarSenha() {
-            var inputPass = document.getElementById('senha');
-            var btnShowPass = document.getElementById('btnSenha');
-
-            if (inputPass.type === 'password') {
-                inputPass.setAttribute('type', 'text');
-                btnShowPass.classList.replace('bi-eye', 'bi-eye-slash');
-            } else {
-                inputPass.setAttribute('type', 'password');
-                btnShowPass.classList.replace('bi-eye-slash', 'bi-eye');
-            }
-        }
-
-        function MostrarSenha2() {
-            var inputPass = document.getElementById('senha1');
-            var btnShowPass = document.getElementById('btnSenha1');
-
-            if (inputPass.type === 'password') {
-                inputPass.setAttribute('type', 'text');
-                btnShowPass.classList.replace('bi-eye', 'bi-eye-slash');
-            } else {
-                inputPass.setAttribute('type', 'password');
-                btnShowPass.classList.replace('bi-eye-slash', 'bi-eye');
-            }
-        }
-
-        document.getElementById('formulario').addEventListener('submit', function(event) {
-            event.preventDefault(); // Impede o envio do formulário
-
-            var senha = document.getElementById('senha').value;
-            var confirmarSenha = document.getElementById('senha1').value;
-            var mensagemErro = document.getElementById('mensagemErro');
-            var mensagemErroSenha = document.getElementById('mensagemErroSenha');
-            var regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
-
-            var formValido = true;
-
-            if (!regex.test(senha)) {
-                formValido = false;
-                mensagemErro.style.display = 'block';
-                mensagemErro.textContent = 'A senha deve ter pelo menos 8 caracteres, uma letra maiúscula, uma letra minúscula, um número e um caractere especial.';
-            } else {
-                mensagemErro.style.display = 'none';
-            }
-
-            if (senha !== confirmarSenha) {
-                formValido = false;
-                mensagemErroSenha.style.display = 'block';
-                mensagemErroSenha.textContent = 'As senhas não coincidem.';
-            } else {
-                mensagemErroSenha.style.display = 'none';
-            }
-
-            if (formValido) {
-                window.location.href = 'personalizar.php'; // Redireciona para a página personalizar.php
-            }
-        });
-    </script>
+    <!--Script para mostrar ou não mostrar a senha-->
+    <script src="../../js/verificarSenha.js"></script>
 
     <script type="text/javascript" src="http://code.jquery.com/jquery-3.0.0.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous" defer></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous" defer>
+    </script>
+    <!--Máscara-->
     <script type="text/javascript" src="../../js/jquery.mask.min.js"></script>
     <script type="text/javascript" src="../../js/personalizar.js"> </script>
 </body>
--- Usuário/Cadastro/process.php
+++ Usuário/Cadastro/process.php
@@ -5,110 +5,67 @@ require_once '../../model/Mensagem.php';
 
 $user = new User();
 $msg = new Mensagem();
-$conexao = new mysqli("localhost", "root", "", "bdcolaai");
 
 //var_dump($_POST);
 switch ($_POST["acao"]) {
-  case 'DELETE':
-    try {
-      $userDao = UserDao::delete($_POST['id']);
-      header("Location: index.php");
-    } catch (Exception $e) {
-      echo 'Exceção capturada: ',  $e->getMensagem(), "\n";
-    }
-    break;
-  case 'SALVAR':
-    $user->setNome($_POST['nomeUsuario']);
-    $user->setSobrenome($_POST['sobrenomeUsuario']);
-    $user->setEmail($_POST['emailUsuario']);
-    $user->setSenha($_POST['senhaUsuario']);
-    $user->setTel($_POST['telUsuario']);
-
-    try { 
-        // Obtém o e-mail enviado pelo formulário
-        $email = $_POST['emailUsuario'];
-    
-        // Consulta SQL para verificar se o e-mail já existe no banco de dados
-        $sql = "SELECT * FROM tbusuario WHERE emailUsuario = '$email'";
-        $resultado = $conexao->query($sql);
-            // Verifica se houve algum erro na execução da consulta
-    if ($resultado === false) {
-      $msg->setMensagem("Erro ao executar a consulta.", "bg-danger");
-      header("Location: index.php");
-     } else {
-        // Verifica se houve algum resultado retornado pela consulta
-        if ($resultado->num_rows > 0) {
-            // E-mail já existe no banco de dados, exibe mensagem de erro
-            $msg->setMensagem("Erro, o e-mail informado já está em uso.", "bg-danger");
-            header("Location: index.php");
-        } else {
-            // E-mail não existe no banco de dados, pode prosseguir com o cadastro
-            $userDao = UserDao::insert($user);
-            $msg->setMensagem("Usuário inserido com sucesso no banco de dados.", "bg-success");
-            header("Location: personalizar.php");
-        }
-    } 
-
-    } catch (Exception $e) {
-      // Se houver um erro na inserção, você pode lidar com isso aqui
-
-      // Adiciona uma mensagem para debug
-      $msg->setMensagem("Erro ao inserir usuário no banco de dados: " . $e->getMensagem(), "bg-danger");
-
-      header("Location: index.php");
-    }
-    break;
-
-  case 'ATUALIZAR':
-    //pode validar as informações
-    $user->setNome($_POST['nomeUsuario']);
-    $user->setSobrenome($_POST['sobrenomeUsuario']);
-    $user->setEmail($_POST['emailUsuario']);
-    $user->setSenha($_POST['senhaUsuario']);
-    $user->setTel($_POST['telUsuario']);
-    $user->setImagemPerfil($user->salvarImagem(($_POST['imagemPerfilUsuario'])));
-    $user->setImagemBanner($user->salvarImagem(($_POST['imagemBannerUsuario'])));
-    try {
-      $userDao = UserDao::update($_POST["idUsuario"], $user);
-      $msg->setMensagem("Usuário atualizado com sucesso.", "bg-success");
-      header("Location: index.php");
-    } catch (Exception $e) {
-      echo 'Exceção capturada: ',  $e->getMensagem(), "\n";
-    }
-    break;
-
-    case 'ATUALIZAR_IMAGEM_PERFIL':
-      // Lógica para atualizar a imagem de perfil do usuário
-      $userId = $_SESSION['idUsuario'];
-      $imagePath = $_FILES['imagemPerfilUsuario']['tmp_name'];
-
-      // Faça o upload da imagem e atualize o perfil do usuário com o caminho da imagem
-      // Exemplo de lógica de upload de imagem:
-      $uploadDir = '../../uploads/perfil/';
-      $imageName = basename($_FILES['imagemPerfilUsuario']['name']);
-      $uploadPath = $uploadDir . $imageName;
-      move_uploaded_file($imagePath, $uploadPath);
-
-      // Atualize o perfil do usuário com o caminho da imagem
-      $user->setId($userId);
-      $user->setImagemPerfil($uploadPath);
-      UserDao::updatePerfilImage($user);
-
-      // Redirecione o usuário para a página de perfil
-      header("Location: perfil.php");
-      exit();
+    case 'DELETE':
+     try {
+          $userDao = UserDao::delete($_POST['id']);
+          header("Location: index.php");
+      } catch (Exception $e) {
+        echo 'Exceção capturada: ',  $e->getMessage(), "\n";
+      }
       break;
+      case 'SALVAR':
+        $user->setNome($_POST['nomeUsuario']); 
+        $user->setSobrenome($_POST['sobrenomeUsuario']);  
+        $user->setEmail($_POST['emailUsuario']);
+        $user->setSenha($_POST['senhaUsuario']);
+        $user->setTel($_POST['telUsuario']);
+        try {
+            $userDao = UserDao::insert($user);
 
-  case 'SELECTID':
+            // Adiciona uma mensagem para debug
+            $msg->setMensagem("Usuário inserido com sucesso no banco de dados.", "bg-success");
 
-    try {
-      $userDao = UserDao::selectById($_POST['id']);
-      // Configura as opções do contexto da solicitação
-      include('register.php');
-    } catch (Exception $e) {
-      echo 'Exceção capturada: ',  $e->getMensagem(), "\n";
-    }
+            header("Location: personalizar.php");
+        } catch (Exception $e) {
+            // Se houver um erro na inserção, você pode lidar com isso aqui
 
+            // Adiciona uma mensagem para debug
+            $msg->setMensagem("Erro ao inserir usuário no banco de dados: " . $e->getMessage(), "bg-danger");
 
-    break;
-}
+            header("Location: index.php");
+        } 
+        break;
+        case 'ATUALIZAR':
+              //pode validar as informações
+              $user->setNome($_POST['nomeUsuario']);
+              $user->setSobrenome($_POST['sobrenomeUsuario']);  
+              $user->setEmail($_POST['emailUsuario']);
+              $user->setSenha($_POST['senhaUsuario']);
+              $user->setTel($_POST['telUsuario']);
+              try {
+                $userDao = UserDao::update($_POST["idUsuario"], $user);
+                $msg->setMensagem("Usuário atualizado com sucesso.", "bg-success");
+                header("Location: index.php");
+              } catch (Exception $e) {
+               echo 'Exceção capturada: ',  $e->getMessage(), "\n";
+      
+              } 
+          break;
+      
+        case 'SELECTID':
+      
+          try {
+              $userDao = UserDao::selectById($_POST['id']);
+              // Configura as opções do contexto da solicitação
+              include('register.php');
+          } catch (Exception $e) {
+              echo 'Exceção capturada: ',  $e->getMessage(), "\n";
+          } 
+      
+        
+          break;
+        }
+?>
