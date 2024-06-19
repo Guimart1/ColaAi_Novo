5aff45f988475c8a5aa0779f9f12297b3e051406
bruno2yah <bruno.geanini04@gmail.com>
2024-06-19 16:44:30 -0300

    banco povoado

--- Usuário/Perfil/index.php
+++ Usuário/Perfil/index.php
@@ -274,7 +274,8 @@ $user = new UserDao();
     <script type="text/javascript" src="../../js/personalizar.js"></script>
     <script type="text/javascript" src="../../js/modal.js"></script>
     <script type="text/javascript" src="../../js/script.js"></script>
-   
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous">
+    </script>
     <script>
         const COMPONENT_NAME = "data-glide";
         const COMPONENT_SELECTOR = `[${COMPONENT_NAME}]`;
